dd6db4f06ed7bd32bdaa34c0d3a34b5875dda818
Willie Chen <willie.chen@jodoinc.com>
2019-07-02 15:20:39 +0800

    update:路由同步到store(未完成)

---
+++ src/history.tsx
@@ -0,0 +1,7 @@
+// 引入一个基于html5 api的history的createBrowserHistory
+import { createBrowserHistory } from 'history';
+
+// 创建一个 history
+const history = createBrowserHistory();
+
+export default history;
--- src/store/reducers/index.tsx
+++ src/store/reducers/index.tsx
@@ -4,12 +4,17 @@
 import { combineReducers } from "redux";
 // 引入需要合并的reducer
 import counter from "./counter";
-// 引入需要合并的reducer
 import counter2 from "./counter2";
+
+// 引入connectRouter
+import { connectRouter } from 'connected-react-router';
+import history from '../../history';
 
 const reducers = combineReducers({
     counter,
-    counter2
+    counter2,
+    // 把history传到connectRouter函数中
+    router: connectRouter(history),
 });
 
 export default reducers;
